9fa219341bfa5a80c6a475ec62a64b31deabb25d
danylott <d.tuytuyshkin@gmail.com>
2020-09-11 17:07:54 +0300

    remove keys.py

--- tests/keys.py
+++
@@ -1,5 +0,0 @@
-# Here we will be storing credentials for all tests
-
-API_KEY = "UcKjc8dreIZCRAHs6rQmmtbjikx7ofAUx9x8mxB2"
-SECRET = "jPug3NNj5l0QJgqoPLLXxrNRJH0cFvRk1vqYsgOY"
-SLACK_WEBHOOK = "https://hooks.slack.com/services///"
